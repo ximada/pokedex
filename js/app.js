7af0621c0880120f2ff9c2ca2a57e03fc04e396e
ximada <witch_ada@hotmail.com>
2018-03-21 01:38:24 -0600

    Cambiando link API

--- js/app.js
+++ js/app.js
@@ -1,46 +1,101 @@
 const galleryPoke = document.getElementById('gallery-poke');
-
-const datapokemon = jsonData => {
-    const pokemon = jsonData.results;
-    printPokeData(pokemon)
-    pokedexPrintInfo(pokemon)
+const invokePokemon = document.getElementById('invoke-pokemon');
+window.addEventListener('load', () => {
+    getJson();
+})
+getJson = (e)=>{
+fetch("https://api.pokemontcg.io/v1/cards/")
+    .then(function (response) {
+        return response.json();
+        
+    })
+    .then(function (data) {
+        //console.log('Request succesful', data);
+        printData(data);
+    })
+    .catch(function (error) {
+        console.log('Request failed', error)
+    });
 }
-
-$.ajax({
-    url: `https://pokeapi.co/api/v2/pokemon-form/`
-}).done(datapokemon)
-
-const printPokeData = jsonData => {
-    console.log(jsonData);
-}
-
-const pokedexPrintInfo = pokeName => { pokeName.forEach(pokemon => {
-    const name = pokemon.name
-   // console.log(name);
-    const createPokemon =`<div class='post text-center'> 
-                           <div class='img-post'>
-                           <img src ="../assets/images/pokemon3.jpg" style="width:50%; border-radius:10%;"/>
-                           </div>
-                           <div class="post-content">
-                               <h3 class="pokemon-name">${name}</h3>
+const printData= data =>{
+  const pokemonsTotal = data.cards;
+  //console.log(pokemonsTotal);
+    pokemonsTotal.forEach(element => {
+        const createPokemon = `<div class='post text-center'> 
+                                <div class='img-post'>
+                                <img src ="${element.imageUrl}" style="width:50%; border-radius:10%;"/>
+                                </div>
+                                <div class="post-content">
+                                <h3 class="pokemon-name">${element.name}</h3>
                                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#poke-info" style="color:white; width:40%;">
-                                  More Info...
+                                   More Info...
                                 </button>
-                          </div>
-                         </div>`;
+                                </div>
+                                </div>`;
+const containerPokedex = document.createElement("div");
+    containerPokedex.classList.add('col-lg-3');
+    containerPokedex.classList.add('d-inline');
+    containerPokedex.classList.add('m-1')
+    containerPokedex.innerHTML= createPokemon;
+    galleryPoke.appendChild(containerPokedex);      
+    });
+    filterPokemon(pokemonsTotal)
+}
+const filterPokemon = (pokemonsTotal)=>{
+    console.log(pokemonsTotal);
+    
+    let pokeNames = pokemonsTotal.name;
+    console.log(pokeNames);
+    
+    // let inputValue = invokePokemon.value.toLowerCase();
+    // console.log(inputValue)
+    // let prueba = pokeNames.filter(pokedx => {
+    //     return pokedx.name.toLowerCase().indexOf(inputValue) >= 0
+    // });
+    
+ }
+invokePokemon.addEventListener('keyup', filterPokemon);
+// const datapokemon = jsonData => {
+//     const pokemon = jsonData.results;
+//     // printPokeData(pokemon)
+//     // pokedexPrintInfo(pokemon)
+// }
+
+// $.ajax({
+//     url: `https://api.pokemontcg.io/v1/cards`
+// }).done(datapokemon)
+
+// const printPokeData = jsonData => {
+//     console.log(jsonData);
+// }
+
+// const pokedexPrintInfo = pokeName => { pokeName.forEach(pokemon => {
+//     const name = pokemon.name
+//    // console.log(name);
+//     const createPokemon =`<div class='post text-center'> 
+//                            <div class='img-post'>
+//                            <img src ="../assets/images/pokemon3.jpg" style="width:50%; border-radius:10%;"/>
+//                            </div>
+//                            <div class="post-content">
+//                                <h3 class="pokemon-name">${name}</h3>
+//                                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#poke-info" style="color:white; width:40%;">
+//                                   More Info...
+//                                 </button>
+//                           </div>
+//                          </div>`;
                         
 
-                const containerPokedex = document.createElement("div");
-                //containerPokedex.classList.add('col');
-                containerPokedex.classList.add('d-inline');
-                containerPokedex.classList.add('m-1')
-                containerPokedex.innerHTML= createPokemon;
-                galleryPoke.appendChild(containerPokedex);
+//                 const containerPokedex = document.createElement("div");
+//                 //containerPokedex.classList.add('col');
+//                 containerPokedex.classList.add('d-inline');
+//                 containerPokedex.classList.add('m-1')
+//                 containerPokedex.innerHTML= createPokemon;
+//                 galleryPoke.appendChild(containerPokedex);
 
-    $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon-form/1`
-    }).done(datapokemon)
-});}
+//     $.ajax({
+//         url: `https://pokeapi.co/api/v2/pokemon-form/1`
+//     }).done(datapokemon)
+// });}
 
 //  function getPokemon(pokemon){
     
